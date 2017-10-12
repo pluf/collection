31bc006ce6066ca59c2c24be7f5d3f0185783138
hadi <hadi@dpq.co.ir>
2017-10-12 17:22:38 +0330

    improve collection to get it by name

--- src/Collection/Shortcuts.php
+++ src/Collection/Shortcuts.php
@@ -8,7 +8,28 @@ function Collection_Shortcuts_NormalizeItemPerPage($request)
     return $count;
 }
 
+/**
+ * Returns collection with given name or throws 404 not found exception if such collection does not exist.
+ * 
+ * @param string $name
+ * @throws Pluf_Exception_DoesNotExist
+ * @return Collection_Collection
+ */
 function Collection_Shortcuts_GetCollectionByNameOr404($name)
+{
+    $collection = Collection_Shortcuts_GetCollectionByName($name);
+    if ($collection === null)
+        throw new Pluf_Exception_DoesNotExist("Collection not found (Collection name:" . $name . ")");
+    return $collection;
+}
+
+/**
+ * Returns collection with given name and returns null if there is no collection with given name.
+ * 
+ * @param string $name
+ * @return Collection_Collection|NULL
+ */
+function Collection_Shortcuts_GetCollectionByName($name)
 {
     $q = new Pluf_SQL('name=%s', array(
         $name
@@ -24,5 +45,5 @@ function Collection_Shortcuts_GetCollectionByNameOr404($name)
         Pluf_Log::error(sprintf('more than one collection exist with the name $s', $name));
         return $item[0];
     }
-    throw new Pluf_Exception_DoesNotExist("Collection not found (Collection name:" . $name . ")");
+    return null;
 }
